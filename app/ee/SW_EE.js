2227ad4cb6850ab83d40cfd30602356140a9c7ff
UPDESKTOP Digital <updesktop@gmail.com>
2020-09-12 00:08:28 -0700

    Update SW_EE.js

--- app/ee/SW_EE.js
+++ app/ee/SW_EE.js
@@ -55,13 +55,36 @@ self.addEventListener('install', async e => {
 self.addEventListener('activate', e => {
   self.clients.claim();
 });
-
+/*
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).catch(function() {
       return caches.match(event.request);
     })
   );
+});
+*/
+self.addEventListener('fetch', event => {
+  // Let the browser do its default thing
+  // for non-GET requests.
+  if (event.request.method != 'GET') return;
+
+  // Prevent the default, and handle the request ourselves.
+  event.respondWith(async function() {
+    // Try to get the response from a cache.
+    const cache = await caches.open(cacheName);
+    const cachedResponse = await cache.match(event.request);
+
+    if (cachedResponse) {
+      // If we found a match in the cache, return it, but also
+      // update the entry in the cache in the background.
+      event.waitUntil(cache.add(event.request));
+      return cachedResponse;
+    }
+
+    // If we didn't find a match in the cache, use the network.
+    return fetch(event.request);
+  }());
 });
 
 async function cacheFirst(req) {
